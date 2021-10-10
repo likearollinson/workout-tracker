300fb9c7d07a890973f1e2d9e0b43b5cee6b8060
Brad R <brad.w.rollinson@gmail.com>
2021-10-10 19:54:25 -0400

    get seeding to work

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // Routes from example application
 
