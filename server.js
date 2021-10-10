c54f428f175ff19a0dda8c06e9580af3f46e1bf9
Brad R <brad.w.rollinson@gmail.com>
2021-10-09 20:30:33 -0400

    add boilerplate server.js file and create a model js file

---
+++ server.js
@@ -0,0 +1,46 @@
+// import required modules
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+//set up port to be used on local server
+const PORT = process.env.PORT || 3000;
+
+const User = require("./workoutModel.js");
+
+// allow express to be used when 'app' variable called
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+
+// Routes from example application
+
+// // Route to post our form submission to mongoDB via mongoose
+// app.post("/submit", ({ body }, res) => {
+//     // Create a new user using req.body
+//     const user = new User(body);
+//     user.setFullName();
+//     user.lastUpdatedDate();
+
+//     User.create(user)
+//         .then(dbUser => {
+//             // If saved successfully, send the the new User document to the client
+//             res.json(dbUser);
+//         })
+//         .catch(err => {
+//             // If an error occurs, send the error to the client
+//             res.json(err);
+//         });
+// });
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
